7c36fc50e84d3a5313687eef7cd2b200fda72368
Rambali Lodhi <95465660+Rambali512@users.noreply.github.com>
2025-07-21 13:26:30 +0000

    conversion operation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let rollNumber = false
+
+
+//console.log(typeof(rollNumber));
+//console.log(typeof rollNumber);
+
+
+//let score = Number(rollNumber);
+
+//console.log(score);
+
+let isLogedin = 31651561
+let booleanLogedin = Boolean(isLogedin);
+console.log(booleanLogedin); 
+
